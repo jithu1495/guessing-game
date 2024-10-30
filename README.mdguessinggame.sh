18b31adaae60c4adf651034c812bf3210e5ff268
jithu1495 <maddinenijitendra@gmail.com>
2024-10-30 10:31:11 +0530

    Create README.mdguessinggame.sh

---
+++ README.mdguessinggame.sh
@@ -0,0 +1,7 @@
+echo "# Guessing Game Project" > README.md
+	echo " " >> README.md
+	echo "## Date and Time of make:" >> README.md
+	date >> README.md
+	echo " " >> README.md
+	echo "## Number of lines in guessinggame.sh:" >> README.md
+	wc -l < guessinggame.sh >> README.md
